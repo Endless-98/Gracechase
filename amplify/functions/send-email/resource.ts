ef276a05eae1fd908a61f65caf0d1a908cc49674
Moses Hoggatt <endlessninetyeight@gmail.com>
2025-10-10 16:31:48 -0600

    Add AWS credentials to Lambda function environment variables

--- amplify/functions/send-email/resource.ts
+++ amplify/functions/send-email/resource.ts
@@ -5,6 +5,8 @@ export const sendEmail = defineFunction({
   entry: "./handler.ts",
   environment: {
     AWS_REGION: "us-east-1",
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID!,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY!,
     FROM_EMAIL: "contact.gracechase@gmail.com",
     TO_EMAIL: "contact.gracechase@gmail.com"
   }
