1844893e9bd229b151bc3cfcefb4b3fe12786c12
Moses Hoggatt <endlessninetyeight@gmail.com>
2025-10-18 16:18:29 -0600

    Merge master into dev: add favicon, Twitter tracking, and Open Graph meta tags

--- src/components/AlbumItem.jsx
+++ src/components/AlbumItem.jsx
@@ -13,7 +13,7 @@ const AlbumItem = ({ spotifyLink, youtubeMusicLink, appleMusicLink }) => {
   return (
       <div className="album-container"> 
         {albumId && (
-          <iframe data-testid="embed-iframe" className="spotify-embed" style={{borderRadius: '12px'}} src={`https://open.spotify.com/embed/album/${albumId}?utm_source=generator`} width="100%" height="352" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy" title="Spotify Album Embed"></iframe>
+          <iframe data-testid="embed-iframe" className="spotify-embed" style={{borderRadius: '12px'}} src={`https://open.spotify.com/embed/album/${albumId}?utm_source=generator`} width="100%" height="352" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
         )}
         <div className="streaming-links">
           {spotifyLink && (
